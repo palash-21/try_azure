ef4800d6c55a66fc895e8d047191de8a58778049
palash-21 <palashpathak.21@gmail.com>
2022-05-03 12:25:07 +0530

    removing port

--- app.py
+++ app.py
@@ -122,4 +122,5 @@ app.add_url_rule(
 )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    #app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(debug=True)
